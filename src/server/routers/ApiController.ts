b84b47375478ed91d87cd75231d66d74624a9a15
刘君杰 <liujunjie@ztebright.com>
2020-09-13 16:04:13 +0800

    增加爬虫代码，为博客内容做准备

--- src/server/routers/ApiController.ts
+++ src/server/routers/ApiController.ts
@@ -20,5 +20,49 @@ class ApiController {
       ...data,
     };
   }
+
+  @route('/blog-list')
+  @GET()
+  async actionBlogList(
+    ctx: Router.IRouterContext,
+  ): Promise<void> {
+    const data = await this.apiService.getBlogList();
+    ctx.body = {
+      ...data,
+    };
+  }
+
+  @route('/blog-categories')
+  @GET()
+  async actionBlogCategories(
+    ctx: Router.IRouterContext,
+  ): Promise<void> {
+    const data = await this.apiService.getBlogCategories();
+    ctx.body = {
+      ...data,
+    };
+  }
+
+  @route('/blog-archives')
+  @GET()
+  async actionBlogArchives(
+    ctx: Router.IRouterContext,
+  ): Promise<void> {
+    const data = await this.apiService.getBlogArchives();
+    ctx.body = {
+      ...data,
+    };
+  }
+
+  @route('/blog-infotypetag')
+  @GET()
+  async actionBlogCurrentInfoAndTypeAndTag(
+    ctx: Router.IRouterContext,
+  ): Promise<void> {
+    const data = await this.apiService.getBlogCurrentInfoAndTypeAndTag();
+    ctx.body = {
+      ...data,
+    };
+  }
 }
 export default ApiController;
